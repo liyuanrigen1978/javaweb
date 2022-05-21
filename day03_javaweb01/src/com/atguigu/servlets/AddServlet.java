704b1aca01fce90214c8d91b656d2af702eb4d01
liyuan1978 <liyuanheluosheng333@hotmail.com>
2022-05-21 17:59:26 +0900

    javaweb

--- day03_javaweb01/src/com/atguigu/servlets/AddServlet.java
+++ day03_javaweb01/src/com/atguigu/servlets/AddServlet.java
@@ -14,6 +14,19 @@ public class AddServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        /*
+        //get方式目前不需要设置编码（基于tomcat8）
+        //如果是get请求发送的中文数据，转码稍微有点麻烦（tomcat8之前）
+        String fname = request.getParameter("fname");
+        //1.将字符串打散成字节数组
+        byte[] bytes = fname.getBytes("ISO-8859-1");
+        //2.将字节数组按照设定的编码重新组装成字符串
+        fname = new String(bytes,"UTF-8");
+        */
+
+        //post方式下，设置编码，防止中文乱码
+        //需要注意的是，设置编码这一句代码必须在所有的获取参数动作之前
+        request.setCharacterEncoding("UTF-8");
         String fname = request.getParameter("fname");
         String priceStr = request.getParameter("price");
         Integer price = Integer.parseInt(priceStr);
